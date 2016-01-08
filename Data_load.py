c32b8bba1e178c9894a7e7b84e436d32090388f6
LuisSoares <luis.miguel.mendes.soares@gmail.com>
2016-01-08 14:45:39 -0500

    New changelist

--- Data_load.py
+++ Data_load.py
@@ -3,7 +3,25 @@ __author__ = 'Luis'
 import numpy as np
 from collections import OrderedDict
 import h5py
+import os
+import sys
 
+PATH=os.path.dirname(os.path.realpath(__file__))
+DEFAULT_TRACK_PATH=os.path.join(PATH,"Features")
+
+if not sys.argv[1]:
+    print('Usage: Data_load <dataset initialization file')
+    sys.exit()
+
+if not os.path.isfile(os.path.join(PATH,sys.argv[1])):
+    print(os.path.isfile(os.path.join(PATH,sys.argv[1])))
+    print ('Please indicate valid dataset file')
+    sys.exit()
+
+if not os.path.exists(os.path.join(PATH,'Datasets')):
+    os.mkdir(os.path.join(PATH,'Datasets')
+
+HDF5_PATH=os.path.join(PATH,'Datasets')
 
 # Genomic stuff
 # TODO: Maybe create a file containing the names of datasets to make it trully costumizable upon deployement
@@ -17,6 +35,16 @@ size_of_chromosomes = [230218, 813184, 316620, 1531933, 576874, 270161, 1090940,
 # Loads chromosomes with track from BDG file
 # the BDG file is a normal BED FILE, the SEQ_NAME collumn should contain the names of the chromosomes without "chr"
 
+def initialization(file_name):
+    datasets=[]
+    with open(file_name) as params_file:
+        header=params_file.readline()
+        assert header=='Dset\tRep\tFile\n'
+        for line in params_file:
+            datasets.append((line[0],line[1],line[2]))
+        return data_sets
+
+
 def load_gene_list(gene_file,name=True):
     gene_list = []
     genes = open(gene_file)
@@ -27,46 +55,53 @@ def load_gene_list(gene_file,name=True):
             gene_list.append([temp[4].strip().upper(), temp[0][3:], int(temp[1]), int(temp[2]), temp[3]])
 
         else:
-            gene_list.append(['CUT', temp[0][3:], int(temp[1]), int(temp[2]), temp[3].strip()])
+            gene_list.append(['ND', temp[0][3:], int(temp[1]), int(temp[2]), temp[3].strip()])
     return gene_list
 
 
-genes = load_gene_list('genes.txt')
-cuts=load_gene_list('CUTS.txt',name=False)
-suts=load_gene_list('suts.txt',name=False)
-orfs=load_gene_list('ORF.txt',name=False)
 
-data_sets_names = ['wt_me3', 'wt_me2','spp1_me3','spp1_me2','swd2_me3','swd2_me2'
-    ,'set2_me3','set2_me2','tbp','h4ac','h3','ncb2']
+
+'''data_sets_names = ['wt_me3', 'wt_me2','spp1_me3','spp1_me2','swd2_me3','swd2_me2'
+    ,'set2_me3','set2_me2','tbp','h4ac','h3','ncb2']'''
 location_sets=['genes','CUTS','SUTS','ORFS']
 
 
-hdf5_file=h5py.File("datasets.hdf5","w")
 
-def create_hdf5_track(dataset_name,hdf5_file_handler):
+
+def create_hdf5_track(dataset_name,dataset_representation,hdf5_file_handler):
     temp=hdf5_file_handler.create_group(dataset_name)
+    temp.attrs['dataset_representation']=dataset_representation
     for n,chr in enumerate(list_of_chromosomes):
         temp.create_dataset(chr,(size_of_chromosomes[n],),dtype='f',compression='gzip', compression_opts=9)
 
 
-def create_location_datatsets():
+def create_location_datatsets(dataset_file):
     for item in location_sets:
-        temp=hdf5_file.create_group(item)
+        temp=dataset_file.create_group(item)
         for n,chr in enumerate(list_of_chromosomes):
             temp.create_dataset(chr,(2,size_of_chromosomes[n],),dtype='b',compression='gzip', compression_opts=9)
 
-def load_track_from_bdg(track_file,name):
-    chromosome_dict = OrderedDict()
-    for n in range(len(list_of_chromosomes)):
-        chromosome_dict[list_of_chromosomes[n]] = np.zeros(size_of_chromosomes[n])
+def load_track_from_bdg(track_file,name,fast=True):
+    """Fast should be used for dense data,
+    slow should be used for large genomes with sparse data
+    TODO: maybe slow is too slow to be usable"""
     with open(track_file) as data:
-        for line in data:
-            # if 'MT' in line:continue
-            line_list = line.rstrip().split('\t')
-            chromosome_dict[line_list[0]][int(line_list[1]):int(line_list[2])] = float(line_list[3])
-    for key,value in chromosome_dict.items():
-            print(key,value)
-            hdf5_file[name][key][...] = value
+        chromosome_dict = OrderedDict()
+        if fast:
+            for n in range(len(list_of_chromosomes)):
+                chromosome_dict[list_of_chromosomes[n]] = np.zeros(size_of_chromosomes[n])
+            for line in data:
+                line_list = line.rstrip().split('\t')
+                chromosome_dict[line_list[0]][int(line_list[1]):int(line_list[2])]=float(line_list[3])
+            for key,value in chromosome_dict.items():
+                print(key,value)
+                hdf5_file[name][key][...] = value
+        else:
+            for line in data:
+                # if 'MT' in line:continue
+                line_list = line.rstrip().split('\t')
+                hdf5_file[name][line_list[0]][int(line_list[1]):int(line_list[2])] = float(line_list[3])
+
 
 def create_genes_tracks(gene_list,name):
     chromosome_dict = OrderedDict()
@@ -81,22 +116,18 @@ def create_genes_tracks(gene_list,name):
             print(key,value)
             hdf5_file[name][key][...] = value
 
-for item in data_sets_names:
-    create_hdf5_track(item,hdf5_file)
-create_location_datatsets()
-load_track_from_bdg('bar40_norm.bdg','wt_me3')
-load_track_from_bdg('bar49_norm.bdg','wt_me2')
-load_track_from_bdg('bar41_norm.bdg','spp1_me3')
-load_track_from_bdg('bar50_norm.bdg','spp1_me2')
-load_track_from_bdg('bar42_norm.bdg','swd2_me3')
-load_track_from_bdg('bar51_norm.bdg','swd2_me2')
-load_track_from_bdg('BAR28_treat_pileup.bdg','set2_me3')
-load_track_from_bdg('BAR29_treat_pileup.bdg','set2_me2')
-load_track_from_bdg('TBP_treat_pileup.bdg','tbp')
-load_track_from_bdg('H4ac_treat_pileup.bdg','h4ac')
-load_track_from_bdg('H3_treat_pileup.bdg','h3')
-load_track_from_bdg('Ncb2_treat_pileup.bdg','ncb2')
-create_genes_tracks(genes,'genes')
-create_genes_tracks(cuts,'CUTS')
-create_genes_tracks(suts,'SUTS')
-create_genes_tracks(orfs,'ORFS')
+hdf5_file=h5py.File(os.path.join("HDF5_PATH",datasets.hdf5","w")
+
+genes = load_gene_list(os.path.join(DEFAULT_TRACK_PATH,'genes.txt'))
+cuts=load_gene_list(os.path.join(DEFAULT_TRACK_PATH,'CUTS.txt'),name=False)
+suts=load_gene_list(os.path.join(DEFAULT_TRACK_PATH,'suts.txt'),name=False)
+orfs=load_gene_list(os.path.join(DEFAULT_TRACK_PATH,'ORF.txt'),name=False)
+
+if __name__ == '__main__':
+    create_location_datatsets()
+    create_genes_tracks(genes,'genes')
+    create_genes_tracks(cuts,'CUTS')
+    create_genes_tracks(suts,'SUTS')
+    create_genes_tracks(orfs,'ORFS')
+
+
